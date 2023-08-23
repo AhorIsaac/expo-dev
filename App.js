338d1dc77b546362fe9ee66a63117f24f6a880f8
AhorIsaac <ahorisaac.ai@gmail.com>
2023-08-23 18:46:25 +0100

    expo: stylesheet, views, text component

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, World!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>
+          It is important to know where you are going than to get there quickly.
+          Do not mistake activity for achievement.
+        </Text>
+        <Text style={styles.boldText}>
+          It is important to know where you are going than to get there quickly.
+          Do not mistake <Text> activity for achievement</Text>.
+        </Text>
+        <Text>
+          It is important to know where you are going than to get there quickly.
+          Do not mistake activity for achievement.
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "#f8f9fa",
+    padding: 12,
   },
 });
