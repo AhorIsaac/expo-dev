491812fe508cc083e93e2acaaaa70a557776d5fc
AhorIsaac <ahorisaac.ai@gmail.com>
2023-08-24 21:43:41 +0100

    expo: lists and scrollview

--- App.js
+++ App.js
@@ -1,49 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 const App = () => {
-  const [name, setName] = useState("Trey");
-  const [age, setAge] = useState(40);
-  const [feedback, setFeedback] = useState("");
+  const [people, setPeople] = useState([
+    { name: "Soovica", key: 1 },
+    { name: "Ecady", key: 2 },
+    { name: "Scarfy", key: 3 },
+    { name: "Faprilla", key: 4 },
+    { name: "Tarli", key: 5 },
+    { name: "Viter", key: 6 },
+    { name: "Chiapre", key: 7 },
+  ]);
 
   return (
     <View style={styles.container}>
-      {/* form group */}
-      <View style={styles.formGroup}>
-        <Text>Name</Text>
-        <TextInput
-          style={styles.input}
-          placeholder={`e.g John Doe`}
-          onChangeText={(val) => setName(val)}
-        />
-      </View>
-
-      {/* form group */}
-      <View style={styles.formGroup}>
-        <Text>Age</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={(val) => setAge(val)}
-          keyboardType="numeric"
-        />
-      </View>
-
-      {/* form group */}
-      <View style={styles.formGroup}>
-        <Text>Feedback</Text>
-        <TextInput
-          multiline
-          style={[styles.input]}
-          onChangeText={(val) => setFeedback(val)}
-          keyboardType="default"
-          placeholder="Enter your feedback"
-        />
-      </View>
-
-      <Text>
-        name: {name}, age: {age}, feedback: {feedback}
-      </Text>
+      <ScrollView>
+        {people.map((item) => {
+          return (
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -53,19 +33,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  formGroup: {
-    width: "100%",
-    paddingHorizontal: 16,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#aaaaaa88",
-    width: "100%",
-    padding: 8,
-    marginVertical: 8,
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "#17a2b8",
+    fontSize: 24,
+    color: "#f8f9fa",
     borderRadius: 8,
   },
 });
